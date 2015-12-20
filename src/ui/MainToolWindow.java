f73e95e91c2c87a4d0f34db2257506c327925b19
Thomas Komarnicki <komarnicki.thomas@gmail.com>
2015-12-20 13:22:18 -0500

    finish moving gps start end emulation to own class

--- src/ui/MainToolWindow.java
+++ src/ui/MainToolWindow.java
@@ -23,42 +23,20 @@ public class MainToolWindow implements ToolWindowFactory, ProgressCallback {
     private Project project;
     private ToolWindow mainToolWindow;
     private JPanel toolWindowContent;
-    private JPanel gpsPathPanel;
-    private JTextField startLocationLat;
-    private JTextField startLocationLon;
+//    private JTextField startLocationLat;
+//    private JTextField startLocationLon;
     private JComboBox mockingToolsComboBox;
-    private JTextField stepsTextField;
-    private JTextField timeIntervalField;
-    private JTextField endLocationLat;
-    private JTextField endLocationLon;
-    private JButton startGPSEmulationButton;
+//    private JTextField stepsTextField;
+//    private JTextField timeIntervalField;
+//    private JTextField endLocationLat;
+//    private JTextField endLocationLon;
+//    private JButton startGPSEmulationButton;
     private JProgressBar progressBar;
-    private JLabel errorLabel;
+    private StartEndEmulation startEndEmulationPanel;
 
     public MainToolWindow(){
-        startGPSEmulationButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                try {
-                    double startLat = Double.valueOf(startLocationLat.getText());
-                    double startLon = Double.valueOf(startLocationLon.getText());
 
-                    double endLat = Double.valueOf(endLocationLat.getText());
-                    double endLon = Double.valueOf(endLocationLon.getText());
-
-                    int steps = Integer.valueOf(stepsTextField.getText());
-                    int timeInterval = Integer.valueOf(timeIntervalField.getText());
-
-                    EmulationService emulationService = ServiceManager.getService(EmulationService.class);
-                    emulationService.setProgressCallback(MainToolWindow.this);
-                    emulationService.emulatePath(startLat, startLon, endLat, endLon, steps * timeInterval * 1000, steps);
-
-                } catch (NumberFormatException e){
-                    e.printStackTrace();
-                }
-            }
-        });
-
+        startEndEmulationPanel.setProgressCallback(this);
         progressBar.setMinimum(0);
         progressBar.setMaximum(100);
     }
@@ -82,4 +60,5 @@ public class MainToolWindow implements ToolWindowFactory, ProgressCallback {
         });
 
     }
+
 }
