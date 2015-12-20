4a9a9f649b41c57acf59ebd169a186fd73e53c12
Thomas Komarnicki <komarnicki.thomas@gmail.com>
2015-12-20 12:39:53 -0500

    start path emulation ui class

---
+++ src/ui/StartEndEmulation.java
@@ -0,0 +1,17 @@
+package ui;
+
+import javax.swing.*;
+
+/**
+ * Created by Thomas on 12/20/2015.
+ */
+public class StartEndEmulation extends JPanel{
+    private JPanel gpsPathPanel;
+    private JTextField startLocationLat;
+    private JTextField startLocationLon;
+    private JButton startGPSEmulationButton;
+    private JTextField stepsTextField;
+    private JTextField timeIntervalField;
+    private JTextField endLocationLat;
+    private JTextField endLocationLon;
+}
